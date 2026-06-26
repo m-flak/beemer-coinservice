6ee41f9ab8ef2f69344f5986e393d3395f34f804
Matthew Kehrer <matthew@kehrer.dev>
2026-06-26 09:00:38 -0500

    azure vps issues

--- run-local.sh
+++ run-local.sh
@@ -5,4 +5,4 @@ AZURE_CLIENT_ID=$(az keyvault secret show --vault-name kv-coinservice --name AZU
 AZURE_CLIENT_SECRET=$(az keyvault secret show --vault-name kv-coinservice --name AZURE-CLIENT-SECRET --query value -o tsv) \
 SPRING_PROFILES_ACTIVE=development,local \
 LOGGING_LEVEL_COM_AZURE=TRACE \
-./gradlew bootRun
+./gradlew bootRun $1
